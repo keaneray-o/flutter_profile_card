ae1a12042c7232cd61253a4452816cf1efb5f432
keaneray-o <rayokeane@gmail.com>
2025-10-23 13:53:33 +0700

    belajar containe, padding, sizebox

--- lib/profile.dart
+++ lib/profile.dart
@@ -7,27 +7,55 @@ class ProfileCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor:  Colors.redAccent,
-        title: const Text('My Profile', 
-        style: TextStyle(
-          color: Colors.white,
-          fontWeight: FontWeight.bold,),),
+        backgroundColor: Colors.redAccent,
+        title: const Text(
+          'My Profile',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
       ),
       body: Center(
-        child:  Column(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Keane Rayo Prasetya', style: TextStyle(
-              fontSize: 20, 
-              fontWeight: FontWeight.bold
+            Container(
+              width:
+                  MediaQuery.of(context).size.width * 0.8, //dalam bentuk persen
+              height: 300,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.amber,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.redAccent,
+                    spreadRadius: 5.0,
+                    blurRadius: 7.0,
+                    offset: Offset(0, 3),
+                  ),
+                ],
               ),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    //only: hanya memmberi satu posisi
+                    //symmetric : memberi jarak sumbu x/y
+                    //all : memberi semua jarak 
+                    child: Text(
+                      'NAME',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Keane Rayo Prasetya',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                ],
               ),
-            Row(children: <Widget>[
-              Column(),
-              Column(),
-            
-            ],
-            )  
+            ),
           ],
         ),
       ),
